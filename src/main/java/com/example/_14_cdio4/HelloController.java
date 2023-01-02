a94e4101217944400c8583f5d451a3f09c504b0d
Kenneth Kaiser <kk1995@live.dk>
2023-01-02 12:10:30 +0100

    Initial commit

---
+++ src/main/java/com/example/_14_cdio4/HelloController.java
@@ -0,0 +1,14 @@
+package com.example._14_cdio4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
