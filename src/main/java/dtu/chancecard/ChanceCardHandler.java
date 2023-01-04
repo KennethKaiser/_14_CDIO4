adc2a4f25ea1d4a33215ccadff6d467421b95c7d
mikkelJurs <s224279@student.dtu.dk>
2023-01-04 10:23:32 +0100

    Added empty methods from whiteboard brainstorm.

--- src/main/java/dtu/chancecard/ChanceCardHandler.java
+++ src/main/java/dtu/chancecard/ChanceCardHandler.java
@@ -1,4 +1,15 @@
 package dtu.chancecard;
 
 public class ChanceCardHandler {
+
+    public void readCSV(){
+
+    }
+    public void shuffleCards(){
+
+    }
+
+    public void deckCreator(){
+
+    }
 }
