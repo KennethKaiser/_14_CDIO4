71739a7da197a0c14281c1ce477e4c3bd1e42e3b
mikkelJurs <s224279@student.dtu.dk>
2023-01-04 11:54:09 +0100

    Added lines to read the chancecardsdesc.csv file.

--- src/main/java/dtu/chancecard/ChanceCardHandler.java
+++ src/main/java/dtu/chancecard/ChanceCardHandler.java
@@ -1,9 +1,13 @@
 package dtu.chancecard;
 
+import dtu.filereader.CSVFileReader;
+
+import java.io.FileReader;
+
 public class ChanceCardHandler {
 
-    public void readCSV(){
-
+    public void readCSVChanceCard(){
+        CSVFileReader.readColon(1, "src/main/java/dtu/chancecard/chancecarddesc.csv", ";");
     }
     public void shuffleCards(){
 
