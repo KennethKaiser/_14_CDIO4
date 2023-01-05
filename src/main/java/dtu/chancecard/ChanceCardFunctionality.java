08c779977238d9f658762f9684dda4b88da451f6
mikkelJurs <s224279@student.dtu.dk>
2023-01-05 12:45:46 +0100

    Added rest of chanceCards. Missing only 3 cards now that is waiting for functionality to be developed. Missing the one where you pay tax from how many houses and hotels you have, and the one that calculates all assets together (Matadorlegatet).

--- src/main/java/dtu/chancecard/ChanceCardFunctionality.java
+++ src/main/java/dtu/chancecard/ChanceCardFunctionality.java
@@ -2,9 +2,11 @@ package dtu.chancecard;
 
 import dtu.players.Player;
 import dtu.players.PlayerHandler;
+import dtu.game.GameActions;
 
 public class ChanceCardFunctionality {
     PlayerHandler playerHandler = new PlayerHandler();
+    GameActions gameActions = new GameActions();
     public void chanceCardFunction(int id, Player player) {
         switch (id) {
             case 0: {
@@ -17,112 +19,100 @@ public class ChanceCardFunctionality {
             }
             case 2: {
                 //De har kørt overfor "rødt lys". Betal 1000 kroner i bøde.
-                player.setMoney(player.getMoney() - 1000);
+                gameActions.changePlayerBalance(player, -1000);
                 break;
             }
             case 3: {
                 //Betal for vognvask og smøring 300,-
-                player.setMoney(player.getMoney() - 300);
+                gameActions.changePlayerBalance(player, -300);
                 break;
             }
             case 4: {
                 //Betal 200,- for levering af 2 kasser øl.
-                player.setMoney(player.getMoney() - 200);
+                gameActions.changePlayerBalance(player, -200);
                 break;
             }
             case 5: {
                 //Betal 3000,- for reparation af deres vogn.
-                player.setMoney(player.getMoney() - 3000);
+                gameActions.changePlayerBalance(player, -3000);
                 break;
             }
             case 6: {
                 //Betal 3000,- for reparation af deres vogn.
-                player.setMoney(player.getMoney() - 3000);
+                gameActions.changePlayerBalance(player, -3000);
                 break;
             }
             case 7: {
                 //De har købt 4 nye dæk til Deres vogn. Betal 1000,-.
-                player.setMoney(player.getMoney() - 1000);
+                gameActions.changePlayerBalance(player, -1000);
                 break;
             }
             case 8: {
                 //De har fået en parkeringsbøde. Betal 200,- i bøde.
-                player.setMoney(player.getMoney() - 200);
+                gameActions.changePlayerBalance(player, -200);
                 break;
             }
             case 9: {
                 //Betal deres bilforsikring 1000,-
-                player.setMoney(player.getMoney() - 1000);
+                gameActions.changePlayerBalance(player, -1000);
                 break;
             }
             case 10: {
                 //De har været udenlands og købt for mange Cult Shaker bedre kendt som "Son of a Bitch".  Betal 200,- i told.
-                player.setMoney(player.getMoney() - 200);
+                gameActions.changePlayerBalance(player, -200);
                 break;
             }
             case 11: {
                 //Tandlægeregning. Betal 2000,-
-                player.setMoney(player.getMoney() - 2000);
+                gameActions.changePlayerBalance(player, -2000);
                 break;
             }
-            case 12: {
-                //De har vundet i klasselotteriet. Modtag 500,-.
-                player.setMoney(player.getMoney() + 500);
-                break;
-            }
+            case 12:
             case 13: {
                 //De har vundet i klasselotteriet. Modtag 500,-.
-                player.setMoney(player.getMoney() + 500);
+                gameActions.changePlayerBalance(player, 500);
                 break;
             }
-            case 14: {
-                //De modtager Deres aktieudbytte. Modtag 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
-                break;
-            }
-            case 15: {
-                //De modtager Deres aktieudbytte. Modtag 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
-                break;
-            }
+            case 14:
+            case 15:
             case 16: {
                 //De modtager Deres aktieudbytte. Modtag 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 17: {
                 //Kommunen har eftergivet et kvartals skat. Hæv i banken 3000,-.
-                player.setMoney(player.getMoney() + 3000);
+                gameActions.changePlayerBalance(player, 3000);
                 break;
             }
             case 18: {
                 //De have en række med elleve rigtige i tipning. Modtag 1000,-.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 19: {
                 //Grundet en veloverstået lønsamtale har De fået lønforhøjelse. Modtag 1000,-.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 20: {
                 //Deres præmieobligation er udtrykket. De modtager 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 21: {
                 //Deres præmieobligation er udtrykket. De modtager 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 22: {
                 //De har solgt nogle gamle møbler på auktion. Modtag 1000,- af banken.
-                player.setMoney(player.getMoney() + 1000);
+                gameActions.changePlayerBalance(player, 1000);
                 break;
             }
             case 23: {
                 //Værdien af egen avl fra nyttehaven udgør 200,- som de modtager af banken.
-                player.setMoney(player.getMoney() + 200);
+                gameActions.changePlayerBalance(player, 200);
                 break;
             }
             case 24: {
@@ -146,72 +136,71 @@ public class ChanceCardFunctionality {
             }
             case 28: {
                 //Ryk frem til START
-                player.setPosition(0);
+                gameActions.movePlayerChanceCard(player, 0);
                 break;
             }
             case 29: {
                 //Ryk frem til START
-                player.setPosition(0);
+                gameActions.movePlayerChanceCard(player, 0);
                 break;
             }
             case 30: {
                 //Ryk tre felter frem
-                player.setPosition(player.getPosition()+3);
+                gameActions.movePlayer(player, 3);
             }
             case 31: {
                 //Ryk tre felter tilbage
-                player.setPosition(player.getPosition()-3);
+                gameActions.movePlayer(player, -3);
                 break;
             }
             case 32: {
                 ////Ryk tre felter tilbage
-                player.setPosition(player.getPosition()-3);
+                gameActions.movePlayer(player, -3);
                 break;
             }
             case 33: {
                 //Ryk frem til Frederiksberg Allé. Hvis De passere START  indkasser da 4000 kr.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.movePlayer(player, 11);
                 break;
             }
             case 34: {
                 //Ryk frem til det nærmeste rederi og betal ejeren to gange den leje han ellers er berettiget til. Hvis selskabet ikke ejes af nogen kan De købe det af banken.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.nearestFerry(player);
                 break;
             }
             case 35: {
                 //Ryk frem til det nærmeste rederi og betal ejeren to gange den leje han ellers er berettiget til. Hvis selskabet ikke ejes af nogen kan De købe det af banken.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.nearestFerry(player);
                 break;
             }
             case 36: {
                 //Tag med Mols-Linien flyt brikken frem og hvis De passerer START indkassér da 4000,-.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.movePlayerChanceCard(player, 15);
                 break;
             }
             case 37: {
                 //Ryk frem til Grønningen hvis De passerer START indkasser da 4000,-.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.movePlayerChanceCard(player, 24);
                 break;
             }
             case 38: {
                 //Ryk frem til Vimmelskaftet hvis de passerer START indkasser da 4000,-.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.movePlayerChanceCard(player, 32);
                 break;
             }
             case 39: {
                 //Tag med den nærmeste færge. Hvis de passerer START indkasser da 4000,-.
-                //awaiting method for moving player over start instead of "teleporting" player.
-
+                gameActions.nearestFerry(player);
                 break;
             }
             case 40: {
                 //Ryk frem til Strandvejen. Hvis De passere START  indkasser da 4000,-.
-                //awaiting method for moving player over start instead of "teleporting" player.
+                gameActions.movePlayerChanceCard(player, 19);
                 break;
             }
             case 41: {
                 //Tag til Rådhuspladsen
-                player.setPosition(39);
+                gameActions.movePlayerChanceCard(player, 39);
                 break;
             }
             case 42: {
