3c9bd20ae88518493372ced64226e19a66cfb4a2
AWG96 <s221094@student.dtu.dk>
2023-01-05 16:25:47 +0100

    Added variables and setters for the PlayerIsBankrupt method

--- src/main/java/dtu/board/FieldProperty.java
+++ src/main/java/dtu/board/FieldProperty.java
@@ -10,6 +10,10 @@ public class FieldProperty extends Field{
     private Boolean owned;
     private Player owner;
 
+    private int activeRent;
+
+    private int buildings;
+
 
     public FieldProperty(Property property){
         this.property = property;
@@ -31,5 +35,26 @@ public class FieldProperty extends Field{
             //En besked fortæller summen af penge der gives og til hvem
             GameActions.payRent(player, property);
         }
+    }
+
+
+    public void setProperty(Property property) {
+        this.property = property;
+    }
+
+    public void setOwned(Boolean owned) {
+        this.owned = owned;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public void setActiveRent(int activeRent) {
+        this.activeRent = activeRent;
+    }
+
+    public void setBuildings(int buildings) {
+        this.buildings = buildings;
     }
 }
