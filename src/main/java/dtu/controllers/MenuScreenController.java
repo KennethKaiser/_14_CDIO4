6cda230bca119d2156ed9742073e0cd92421554f
Toby <tobiasgoerlyk@hotmail.com>
2023-01-06 10:26:26 +0100

    MVP Main Menu

--- src/main/java/dtu/controllers/MenuScreenController.java
+++ src/main/java/dtu/controllers/MenuScreenController.java
@@ -1,9 +1,6 @@
 package dtu.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
@@ -29,21 +26,64 @@ public class MenuScreenController {
     HBox player5;
     @FXML
     HBox player6;
-
+    @FXML
+    Text name1;
+    @FXML
+    Text name2;
+    @FXML
+    Text name3;
+    @FXML
+    Text name4;
+    @FXML
+    Text name5;
+    @FXML
+    Text name6;
+    @FXML
+    ImageView car1;
+    @FXML
+    ImageView car2;
+    @FXML
+    ImageView car3;
+    @FXML
+    ImageView car4;
+    @FXML
+    ImageView car5;
+    @FXML
+    ImageView car6;
+    @FXML
+    Button button1;
+    @FXML
+    Button button2;
+    @FXML
+    Button button3;
+    @FXML
+    Button button4;
+    @FXML
+    Button button5;
+    @FXML
+    Button button6;
     @FXML
     ComboBox colorPicker;
     @FXML
     TextField nameInput;
 
     int playersAdded;
+    String[] playerNamesAdded;
+    Image[] playerColorsAdded;
     String[] colors;
     String[] removedColors;
     HBox[] players;
     Image[] carImages;
+    ImageView[] cars;
+    Text[] names;
     Button[] buttons;
     int[] numbers;
+
+
+
+
     @FXML
-    public void initialize(){
+    public void initialize() {
         carImages = new Image[6];
         carImages[0] = image("src/textures/greenCar.png");
         carImages[1] = image("src/textures/blueCar.png");
@@ -65,14 +105,37 @@ public class MenuScreenController {
         players[3] = player4;
         players[4] = player5;
         players[5] = player6;
+        for(int i = 0; i < players.length; i++){
+            players[i].setOpacity(0);
+        }
         numbers = new int[6];
         removedColors = new String[6];
-        for(int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
             numbers[i] = i;
             removedColors[i] = "";
         }
         updateColorPicker();
         buttons = new Button[6];
+        buttons[0] = button1;
+        buttons[1] = button2;
+        buttons[2] = button3;
+        buttons[3] = button4;
+        buttons[4] = button5;
+        buttons[5] = button6;
+        names = new Text[6];
+        names[0] = name1;
+        names[1] = name2;
+        names[2] = name3;
+        names[3] = name4;
+        names[4] = name5;
+        names[5] = name6;
+        cars = new ImageView[6];
+        cars[0] = car1;
+        cars[1] = car2;
+        cars[2] = car3;
+        cars[3] = car4;
+        cars[4] = car5;
+        cars[5] = car6;
     }
     public Image image(String url){
         try{
@@ -111,83 +174,71 @@ public class MenuScreenController {
         }
     }
     public void addPlayerPressed(){
-        String name = nameInput.getText();
-        String color = (String) colorPicker.getValue();
-        removeColor(color);
-        Text nameText = new Text(name);
-        ImageView carImage = new ImageView();
-        carImage.setImage(getCarImage(color));
-        carImage.setFitWidth(30);
-        carImage.setFitHeight(30);
-        Button button = new Button("X");
-        button.setOnAction(e -> removePlayer(playersAdded+1));
-        players[playersAdded].getChildren().clear();
-        players[playersAdded].getChildren().addAll(nameText, carImage, button);
-        playersAdded++;
-        buttons[playersAdded] = button;
-        updateColorPicker();
-    }
-    private void removePlayer(int player){
-        if(player < playersAdded){
-            players[player].getChildren().clear();
-            players[player].getChildren().addAll(players[player+1]);
-            buttons[player] = buttons[player+1];
-            if(player+1 < playersAdded){
-                players[player+1].getChildren().clear();
-                players[player+1].getChildren().addAll(players[player+2]);
-                buttons[player+1] = buttons[player+2];
-                if(player+2 < playersAdded){
-                    players[player+2].getChildren().clear();
-                    players[player+2].getChildren().addAll(players[player+3]);
-                    buttons[player+2] = buttons[player+3];
-                    if(player+3 < playersAdded){
-                        players[player+3].getChildren().clear();
-                        players[player+3].getChildren().addAll(players[player+4]);
-                        buttons[player+3] = buttons[player+4];
-                        if(player+4 < playersAdded){
-                            players[player+4].getChildren().clear();
-                            players[player+4].getChildren().addAll(players[player+5]);
-                            players[player+5].getChildren().clear();
-                            buttons[player+4] = buttons[player+5];
-                        }
-                        else players[player+4].getChildren().clear();
-                    }
-                    else players[player+3].getChildren().clear();
+        if(playersAdded < 6){
+            String name = nameInput.getText();
+            String color = colorPicker.getValue().toString();
+            removeColor(color);
+            for(int i = 0; i < players.length; i++){
+                if(players[i].getOpacity() < 1){
+                    setPlayer(i,name,color);
+                    break;
                 }
-                else players[player+2].getChildren().clear();
             }
-            else players[player+1].getChildren().clear();
+            playersAdded++;
         }
         else{
-            players[player].getChildren().clear();
+            System.out.println("Cannot add another player, maximum has been created");
         }
-        for(int i = 0; i < buttons.length; i++){
-            int temp = i;
-            if(buttons[i] != null){
-                buttons[i].setOnAction(e -> removePlayer(temp));
+    }
+    private void setPlayer(int playerNumber, String name, String color){
+        players[playerNumber].setOpacity(1);
+        names[playerNumber].setText(name);
+        cars[playerNumber].setImage(getCarImage(color));
+        buttons[playerNumber].setOnAction(e -> removePlayer(playerNumber, color));
+    }
+    private void removePlayer(int playerNumber, String colorToReturn){
+        for(int i = 0; i < colors.length;i++){
+            if(colors[i] == "") {
+                colors[i] = colorToReturn;
+                break;
             }
         }
+        if(playerNumber < playersAdded-1){
+            moveDown(playerNumber+1, colorToReturn);
+        }
+        else{
+            players[playerNumber].setOpacity(0);
+        }
+        playersAdded--;
         updateColorPicker();
+    }
+    private void moveDown(int playerNumber, String color){
+        names[playerNumber-1].setText(names[playerNumber].getText());
+        cars[playerNumber-1].setImage(cars[playerNumber].getImage());
+        buttons[playerNumber-1].setOnAction(e -> removePlayer(playerNumber-1, color));
+        if(playerNumber < playersAdded-1){
+            moveDown(playerNumber+1, color);
+        }
+        else{
+            players[playerNumber].setOpacity(0);
+        }
     }
     void removeColor(String color){
         colorPicker.getItems().removeAll();
         for (int i = 0; i < colors.length; i++){
             if(colors[i].equals(color)){
                 colors[i] = "";
-                for(int n = 0; n < removedColors.length; n++){
-                    if(removedColors[n] == ""){
-                        removedColors[n] = color;
-                        break;
-                    }
-                }
-
             }
         }
-
+        updateColorPicker();
     }
-
     public void startGamePressed(){
-
+        playerColorsAdded = new Image[playersAdded];
+        playerNamesAdded = new String[playersAdded];
+        for(int i = 0; i < playersAdded; i++){
+            playerColorsAdded[i] = cars[i].getImage();
+            playerNamesAdded[i] = names[i].getText();
+        }
     }
 
 }
