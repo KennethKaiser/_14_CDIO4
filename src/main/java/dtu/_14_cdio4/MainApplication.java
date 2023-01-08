536b2c4bb356f80268b68f817c22d637c388b7df
Toby <tobiasgoerlyk@hotmail.com>
2023-01-08 12:08:54 +0100

    You can now switch scenes and it starts in the menu

--- src/main/java/dtu/_14_cdio4/MainApplication.java
+++ src/main/java/dtu/_14_cdio4/MainApplication.java
@@ -13,13 +13,6 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
 
-    private BoardController boardController;
-    private ChanceCardsController chanceCardsController;
-    private PlayerViewController playerViewController;
-    private PropertyMenuController propertyMenuController;
-    private StartGameController startGameController;
-    private TradingMenuController tradingMenuController;
-    private MenuScreenController menuScreenController;
     @Override
     public void start(Stage stage) throws IOException {
         SceneSwitch sceneSwitch = new SceneSwitch(stage);
