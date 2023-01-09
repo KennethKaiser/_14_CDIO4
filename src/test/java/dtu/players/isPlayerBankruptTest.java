8a945fa947ff0689e4e71e80c40a0c9fcef7684d
AWG96 <s221094@student.dtu.dk>
2023-01-09 14:38:11 +0100

    Modified test of isPlayerBankruptTest

--- src/test/java/dtu/players/isPlayerBankruptTest.java
+++ src/test/java/dtu/players/isPlayerBankruptTest.java
@@ -11,8 +11,8 @@ class isPlayerBankruptTest {
         PlayerHandler playerHandler = new PlayerHandler();
         playerHandler.initializePlayers(3);
         playerHandler.initializePlayerInPlayers(0,"Niels", 4000, "Black");
-        playerHandler.initializePlayerInPlayers(1, "tis", 4000, "blue");
-        playerHandler.initializePlayerInPlayers(2, "hejsa", 4000, "red");
+        playerHandler.initializePlayerInPlayers(1, "Karl", 4000, "blue");
+        playerHandler.initializePlayerInPlayers(2, "Hans", 4000, "red");
 
         playerHandler.getPlayers()[0].setMoney(-1);
 
