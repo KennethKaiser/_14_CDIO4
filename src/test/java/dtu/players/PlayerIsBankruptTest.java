2f650e489498b26cd9438d43463d0c9f974330ed
AWG96 <s221094@student.dtu.dk>
2023-01-09 14:07:38 +0100

    Added method to test if player loses properties on bankrupt, not final

---
+++ src/test/java/dtu/players/PlayerIsBankruptTest.java
@@ -0,0 +1,36 @@
+package dtu.players;
+
+import dtu.board.Board;
+import dtu.board.FieldProperty;
+import dtu.board.Property;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+class PlayerIsBankruptTest {
+
+    Board board = new Board();
+
+    @Test
+    void playerIsBankruptTest(Player player) {
+
+        PlayerHandler playerHandler = new PlayerHandler();
+
+        player.setMoney(-1);
+        FieldProperty property = new FieldProperty(board.getCurrentBoard()[1].getProperty());
+        player.setProperties(property);
+
+        playerHandler.playerIsBankrupt(player);
+
+        assertEquals()
+
+
+
+
+
+
+    }
+}
+
+ */
