f3f4078bb1068f3725015e9861d908d4abbe9d6e
AWG96 <s221094@student.dtu.dk>
2023-01-09 11:22:48 +0100

    Test of method isPlayerBankrupt

---
+++ src/test/java/dtu/players/isPlayerBankruptTest.java
@@ -0,0 +1,23 @@
+package dtu.players;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+class isPlayerBankruptTest {
+
+    @Test
+    void isPlayerBankruptTest() {
+        Player player = new Player(1,"Niels", 4000, "Black");
+        PlayerHandler playerHandler = new PlayerHandler();
+        player.setMoney(-1);
+        playerHandler.isPlayerBankrupt(player);
+        assertEquals(true, playerHandler.isPlayerBankrupt(player));
+
+
+    }
+
+
+}
+*/
