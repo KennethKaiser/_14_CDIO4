38679c018794f81592a88dca81d8094fc48f9ec0
AWG96 <s221094@student.dtu.dk>
2023-01-09 15:19:56 +0100

    Modified PlayerIsBankruptTest

--- src/test/java/dtu/players/PlayerIsBankruptTest.java
+++ src/test/java/dtu/players/PlayerIsBankruptTest.java
@@ -1,36 +1,37 @@
 package dtu.players;
 
 import dtu.board.Board;
+import dtu.board.Field;
 import dtu.board.FieldProperty;
 import dtu.board.Property;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-/*
+
 class PlayerIsBankruptTest {
 
-    Board board = new Board();
-
     @Test
-    void playerIsBankruptTest(Player player) {
-
+    void playerIsBankruptTest() {
+        ArrayList<Field> properties = new ArrayList<>();
         PlayerHandler playerHandler = new PlayerHandler();
+        Board board = new Board();
 
-        player.setMoney(-1);
-        FieldProperty property = new FieldProperty(board.getCurrentBoard()[1].getProperty());
-        player.setProperties(property);
+        playerHandler.initializePlayers(2);
+        playerHandler.initializePlayerInPlayers(0, "Test1", 4000, "black");
+        playerHandler.initializePlayerInPlayers(1, "Test2", 4000, "blue");
 
-        playerHandler.playerIsBankrupt(player);
+        properties.add(board.getCurrentBoard()[1]);
+        playerHandler.getPlayers()[0].setProperties(properties);
+        FieldProperty fieldProperty = (FieldProperty) playerHandler.getPlayers()[0].getProperties().get(0);
+        fieldProperty.setOwner(playerHandler.getPlayers()[0]);
+        fieldProperty.setOwned(true);
+        fieldProperty.setActiveRent(500);
 
-        assertEquals()
-
-
-
-
-
-
+        playerHandler.playerIsBankrupt(playerHandler.getPlayers()[0]);
+        assertEquals(false, playerHandler.getPlayers()[0].isBankrupt());
     }
 }
 
- */
