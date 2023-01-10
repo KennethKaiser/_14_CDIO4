7c6a4fdf3dd9aeb6e170aab9a17939a615f5073e
Kenneth Kaiser <kk1995@live.dk>
2023-01-10 14:41:46 +0100

    Created a simple sum calculator in RaffleCup.

--- src/main/java/dtu/dice/RaffleCup.java
+++ src/main/java/dtu/dice/RaffleCup.java
@@ -6,6 +6,7 @@ public class RaffleCup {
 
     private final int NUMBER_OF_ROLLS = 2;
     private int[] ourRolls;
+    private int sum;
     private Die die = new Die();
 
     /**
@@ -20,10 +21,20 @@ public class RaffleCup {
         for(int i =0; i < ourRolls.length; i++){
             ourRolls[i] = random.nextInt(die.getMAXIMUM()-die.getMINIMUM()+1)+1;
         }
+        turnSum(ourRolls);
         return ourRolls;
     }
 
+    public void turnSum(int[] lastRoll){
+        sum = lastRoll[0] + lastRoll[1];
+    }
+
+
     public int[] getOurRolls() {
         return ourRolls;
+    }
+
+    public int getSum() {
+        return sum;
     }
 }
