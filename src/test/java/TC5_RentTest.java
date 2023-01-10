51bc08c94a8df18da72dc316e126f1e708d35394
Nicklas <Nicklas-Christensen@outlook.dk>
2023-01-10 16:51:18 +0100

    Har fixet Rent unit-testen

--- src/test/java/TC5_RentTest.java
+++ src/test/java/TC5_RentTest.java
@@ -1,134 +1,108 @@
+import dtu._14_cdio4.BoardController1;
 import dtu.board.FieldProperty;
 import dtu.board.Property;
 import dtu.board.PropertyList;
 import dtu.board.PropertyHandler;
 import dtu.board.Board;
+import dtu.controllers.BoardController;
+import dtu.controllers.ControllerHandler;
 import dtu.players.Player;
+import dtu.players.PlayerHandler;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class TC5_RentTest {
 
-    PropertyHandler propertyHandler = new PropertyHandler();
-    Board board = new Board();
-
     @Test
     void testPayRent() {
         //Variable
-        int RENT = 400;
+        int RENT = 50;
         final int START_MONEY = 4000;
+        final int PRICE = 1200;
+        BoardController boardController = new BoardController();
+        Board board = new Board();
+        PlayerHandler playerHandler = new PlayerHandler();
         //Expected
         int lost = START_MONEY - RENT;
-        int gained = START_MONEY + RENT;
+        int gained = (START_MONEY - PRICE) + RENT;
 
-        Player player1 = new Player(0,"Nicklas",START_MONEY,"Black");
-        Player player2 = new Player(1,"Andreas",START_MONEY,"Red");
+        playerHandler.initializePlayers(3);
+        playerHandler.initializePlayerInPlayers(0,"Niels", START_MONEY, "black");
+        playerHandler.initializePlayerInPlayers(1, "Karl", START_MONEY, "blue");
+        playerHandler.initializePlayerInPlayers(2, "Hans", START_MONEY, "red");
 
-        Property property = new Property(0, 1,"DTU",300,200,RENT,500,600,700,800,900,5);
-        Property property2 = new Property(1, 1,"Bornholm",300,200,RENT,500,600,700,800,900,5);
 
-        FieldProperty propertyField = new FieldProperty(property);
-        propertyField.setOwner(player2);
-        propertyField.setOwned(true);
-        propertyField.setActiveRent(0);
-        FieldProperty propertyField2 = new FieldProperty(property2);
-        propertyField2.setOwner(player1);
-        propertyField2.setOwned(true);
-        propertyField2.setActiveRent(0);
+        FieldProperty fieldProperty1 = (FieldProperty) board.getCurrentBoard()[1];
+        fieldProperty1.buy(playerHandler.getPlayers()[0]);
+        fieldProperty1.rent(playerHandler.getPlayers()[1], fieldProperty1.getProperty().getRentNormal());
 
-        propertyField.rent(player1);
 
         //Tester at den ene spiller mistede penge og den anden fik
-        assertTrue((player1.getMoney() == lost) && (player2.getMoney() == gained), "expect player1 to have: " + lost + " and player2 to have: " + gained + ". player1 has " + player1.getMoney()
-                + ". And player 2 has: "+ player2.getMoney());
-        System.out.println("expect player1 to have: " + lost + ", player1 has: " + player1.getMoney() + ". Expect player2 to have: " + gained + ", player 2 has: " + player2.getMoney());
+        assertEquals(gained,playerHandler.getPlayers()[0].getMoney(),"expect player1 to have: " + gained + ". player1 has " + playerHandler.getPlayers()[0].getMoney());
+        assertEquals(lost,playerHandler.getPlayers()[1].getMoney(),"expect player2 to have: " + lost + ". Player 2 has: "+ playerHandler.getPlayers()[1].getMoney());
     }
 
     @Test
     void testPay2XRent() {
         //Variable
-        int RENT = 400;
+        int RENT = 50;
         final int START_MONEY = 4000;
+        final int PRICE = 1200;
+        BoardController boardController = new BoardController();
+        Board board = new Board();
+        PlayerHandler playerHandler = new PlayerHandler();
         //Expected
         int lost = START_MONEY - (RENT * 2);
-        int gained = START_MONEY + (RENT * 2);
+        int gained = (START_MONEY - (PRICE * 2)) + (RENT * 2);
 
-        Player player1 = new Player(0,"Nicklas",START_MONEY,"Black");
-        Player player2 = new Player(1,"Andreas",START_MONEY,"Red");
+        playerHandler.initializePlayers(3);
+        playerHandler.initializePlayerInPlayers(0,"Niels", START_MONEY, "black");
+        playerHandler.initializePlayerInPlayers(1, "Karl", START_MONEY, "blue");
+        playerHandler.initializePlayerInPlayers(2, "Hans", START_MONEY, "red");
 
-        Property property = new Property(0, 1,"DTU",300,200,RENT,500,600,700,800,900,5);
-        Property property2 = new Property(1, 1,"Bornholm",300,200,RENT,500,600,700,800,900,5);
 
-        FieldProperty propertyField = new FieldProperty(property);
-        propertyField.setOwner(player2);
-        propertyField.setOwned(true);
-        propertyField.setActiveRent(0);
-        FieldProperty propertyField2 = new FieldProperty(property2);
-        propertyField2.setOwner(player2);
-        propertyField2.setOwned(true);
-        propertyField2.setActiveRent(0);
+        FieldProperty fieldProperty1 = (FieldProperty) board.getCurrentBoard()[1];
+        FieldProperty fieldProperty2 = (FieldProperty) board.getCurrentBoard()[3];
+        fieldProperty1.buy(playerHandler.getPlayers()[0]);
+        fieldProperty2.buy(playerHandler.getPlayers()[0]);
+        fieldProperty1.rent(playerHandler.getPlayers()[1], fieldProperty1.getProperty().getRentNormal());
 
-        propertyField.rent(player1);
 
         //Tester at den ene spiller mistede penge og den anden fik
-        assertTrue((player1.getMoney() == lost) && (player2.getMoney() == gained), "expect player1 to have: " + lost + " and player2 to have: " + gained + ". player1 has " + player1.getMoney()
-                + ". And player 2 has: "+ player2.getMoney());
-        System.out.println("expect player1 to have: " + lost + ", player1 has: " + player1.getMoney() + ". Expect player2 to have: " + gained + ", player 2 has: " + player2.getMoney());
-    }
-
-    @Test
-    void testPay0Rent() {
-        //Variable
-        int RENT = 0;
-        final int START_MONEY = 4000;
-        //Expected
-        int lost = START_MONEY - RENT;
-        int gained = START_MONEY + RENT;
-
-        Player player1 = new Player(0,"Nicklas",START_MONEY,"Black");
-        Player player2 = new Player(1,"Andreas",START_MONEY,"Red");
-
-        Property property = new Property(0,1,"DTU",300,200,RENT,500,600,700,800,900,5);
-
-        FieldProperty propertyField = new FieldProperty(property);
-        propertyField.setOwner(player2);
-        propertyField.setOwned(true);
-        propertyField.setActiveRent(0);
-
-        propertyField.rent(player1);
-
-        //Testede om systemet virkede med værdier på 0
-        assertTrue((player1.getMoney() == lost) && (player2.getMoney() == gained), "expect player1 to have: " + lost + " and player2 to have: " + gained + ". player1 has " + player1.getMoney()
-                + ". And player 2 has: "+ player2.getMoney());
-        System.out.println("expect player1 to have: " + lost + ", player1 has: " + player1.getMoney() + ". Expect player2 to have: " + gained + ", player 2 has: " + player2.getMoney());
+        assertEquals(gained,playerHandler.getPlayers()[0].getMoney(),"expect player1 to have: " + gained + ". player1 has " + playerHandler.getPlayers()[0].getMoney());
+        assertEquals(lost,playerHandler.getPlayers()[1].getMoney(),"expect player2 to have: " + lost + ". Player 2 has: "+ playerHandler.getPlayers()[1].getMoney());
     }
 
     @Test
     void testPayRent1House() {
         //Variable
-        int RENT_HOUSE = 450;
+        int RENT = 250;
         final int START_MONEY = 4000;
+        final int PRICE = 1200;
+        BoardController boardController = new BoardController();
+        Board board = new Board();
+        PlayerHandler playerHandler = new PlayerHandler();
         //Expected
-        int lost = START_MONEY - RENT_HOUSE;
-        int gained = START_MONEY + RENT_HOUSE;
+        int lost = START_MONEY - RENT;
+        int gained = (START_MONEY - PRICE) + RENT;
 
-        Player player1 = new Player(0,"Nicklas",START_MONEY,"Black");
-        Player player2 = new Player(1,"Andreas",START_MONEY,"Red");
+        playerHandler.initializePlayers(3);
+        playerHandler.initializePlayerInPlayers(0,"Niels", START_MONEY, "black");
+        playerHandler.initializePlayerInPlayers(1, "Karl", START_MONEY, "blue");
+        playerHandler.initializePlayerInPlayers(2, "Hans", START_MONEY, "red");
 
-        Property property = new Property(0,1,"DTU",300,200,200,RENT_HOUSE,600,700,800,900,5);
 
-        FieldProperty propertyField = new FieldProperty(property);
-        propertyField.setOwner(player2);
-        propertyField.setOwned(true);
-        propertyField.setActiveRent(1);
+        FieldProperty fieldProperty1 = (FieldProperty) board.getCurrentBoard()[1];
+        fieldProperty1.buy(playerHandler.getPlayers()[0]);
+        fieldProperty1.setBuildings(1);
+        fieldProperty1.rent(playerHandler.getPlayers()[1], fieldProperty1.getProperty().getRent1House());
 
-        propertyField.rent(player1);
 
-        //Tester at lejen også ændres hvis der er et hus
-        assertTrue((player1.getMoney() == lost) && (player2.getMoney() == gained), "expect player1 to have: " + lost + " and player2 to have: " + gained + ". player1 has " + player1.getMoney()
-                + ". And player 2 has: "+ player2.getMoney());
-        System.out.println("expect player1 to have: " + lost + ", player1 has: " + player1.getMoney() + ". Expect player2 to have: " + gained + ", player 2 has: " + player2.getMoney());
+        //Tester at den ene spiller mistede penge og den anden fik
+        assertEquals(gained,playerHandler.getPlayers()[0].getMoney(),"expect player1 to have: " + gained + ". player1 has " + playerHandler.getPlayers()[0].getMoney());
+        assertEquals(lost,playerHandler.getPlayers()[1].getMoney(),"expect player2 to have: " + lost + ". Player 2 has: "+ playerHandler.getPlayers()[1].getMoney());
     }
 
 }
