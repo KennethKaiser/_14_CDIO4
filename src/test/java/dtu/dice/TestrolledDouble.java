e1bbbbb18b2c5fc7db4d43e49318c35fc04627bb
AWG96 <s221094@student.dtu.dk>
2023-01-11 14:21:50 +0100

    Added test of rolledDouble method

---
+++ src/test/java/dtu/dice/TestrolledDouble.java
@@ -0,0 +1,31 @@
+package dtu.dice;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestrolledDouble {
+
+    @Test
+    void TestrolledDouble() {
+
+        RaffleCup raffleCup = new RaffleCup();
+
+        raffleCup.roll();
+        raffleCup.setOurRolls(new int[]{1, 1});
+        raffleCup.rolledDouble();
+        System.out.println(raffleCup.getNumberOfDoubles());
+        System.out.println(raffleCup.getOurRolls()[0]);
+        System.out.println(raffleCup.getOurRolls()[1]);
+
+        raffleCup.roll();
+        raffleCup.setOurRolls(new int[]{2, 2});
+        raffleCup.rolledDouble();
+        System.out.println(raffleCup.getNumberOfDoubles());
+        System.out.println(raffleCup.getOurRolls()[0]);
+        System.out.println(raffleCup.getOurRolls()[1]);
+        assertEquals(true, raffleCup.rolledDouble());
+    }
+}
