0819a57d6079e5af5d9ca66aec21d23c59765dbb
Nicklas <Nicklas-Christensen@outlook.dk>
2023-01-12 15:06:26 +0100

    Har lavet en test for Chance class

---
+++ src/test/java/dtu/board/DummyTest.java
@@ -0,0 +1,36 @@
+package dtu.board;
+
+import dtu.chancecard.ChanceCardDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//Skal muligvis ændres hvis Parkering giver penge sum
+class DummyTest {
+
+    @Test
+    void LabelAndType() {
+       final String LABEL = "Du er landet på prøv lykken";
+       final String TYPE = "chance";
+
+       Chance chance = new Chance();
+
+       //Tester at label på Chance er rigtigt
+        assertEquals(LABEL, chance.landedLabel(),"Expect Chance to have label: "+ LABEL +". Chance has label: "+chance.landedLabel());
+        //Tester at Type på chance er rigtigt
+        assertEquals(TYPE, chance.type(),"Expect chance to have type: "+ TYPE +". Chance has type as: "+ chance.type());
+
+    }
+
+    @Test
+    void DrawCard() {
+        String card;
+        ChanceCardDeck chanceCardDeck = new ChanceCardDeck();
+        Chance chance = new Chance();
+        card = chance.getChanceCardDeck().getDeck()[0];
+
+        //Tester at kortet der trækkes er det første i bunken
+        assertEquals(card, chance.drawCard(),"Expect Chance to draw card: "+ card +". Chance draws card: "+chance.drawCard());
+    }
+
+}
