27679b525b33d19f2b6c324a9aa4da4ea9c3665b
Toby <tobiasgoerlyk@hotmail.com>
2023-01-16 13:32:02 +0100

    Players can give up

--- src/main/java/dtu/controllers/PauseMenuController.java
+++ src/main/java/dtu/controllers/PauseMenuController.java
@@ -1,6 +1,7 @@
 package dtu.controllers;
 
 import dtu.SaveAndLoad.SaveAndLoad;
+import dtu.players.Player;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -17,14 +18,36 @@ public class PauseMenuController {
     Button exitNoSaveButton;
     @FXML
     Button resumeButton;
+    @FXML
+    Button giveUpButton;
+
 
     SaveAndLoad saveAndLoad = new SaveAndLoad();
+    Player playerCheck = null;
 
     @FXML
     public void initialize(){
         resumeButton.setOnAction(e -> resumeGame());
         exitNoSaveButton.setOnAction(e -> exitWithoutSaving());
         saveAsButton.setOnAction(e -> saveAs());
+    }
+    public void open(){
+        Player playerToGiveUp = ControllerHandler.getInstance().getBoardController().playerHandler.getCurrentPlayer();
+
+        giveUpButton.setText("Giv op for " + playerToGiveUp.getName());
+        giveUpButton.setOnAction(e -> giveUpPlayer(playerToGiveUp));
+    }
+    private void giveUpPlayer(Player player){
+        if(playerCheck == null){
+            playerCheck = player;
+            sendText("Er du sikker på at du vil opgive " + player.getName());
+        }
+        else if(playerCheck == player){
+            sendText("Gav alle " + player.getName() + "'s grunde tilbage til banken");
+            player.setBankrupt(true);
+            ControllerHandler.getInstance().getBoardController().playerHandler.playerIsBankrupt(player);
+            ControllerHandler.getInstance().getBoardController().endTurnAfterBankrupt();
+        }
     }
     public void saveAs(){
         if(!saveInput.getText().equals("")){
