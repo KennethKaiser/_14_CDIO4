4f6d2e6899385cf309156ec08686d8d80822d357
AWG96 <s221094@student.dtu.dk>
2023-01-17 16:39:30 +0100

    Clean-Up of PauseMenuController.java

--- src/main/java/dtu/controllers/PauseMenuController.java
+++ src/main/java/dtu/controllers/PauseMenuController.java
@@ -6,11 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
-import javafx.stage.WindowEvent;
+
 
 public class PauseMenuController {
 
